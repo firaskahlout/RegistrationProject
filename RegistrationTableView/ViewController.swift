46e40a0b418618caa0d5d3f59921f60ae2c22aa4
firas <firaskahlout@gmail.com>
2019-10-07 10:14:47 +0300

    Initial Commit

---
+++ RegistrationTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RegistrationTableView
+//
+//  Created by IFone on 10/7/19.
+//  Copyright © 2019 Firas Alkahlout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
